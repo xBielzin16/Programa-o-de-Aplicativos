763815672c27c37cb05a352b6c1345d72b0375d5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 14:46:16 -0300

    o que você fez

---
+++ 6.switch-case.js
@@ -0,0 +1,22 @@
+let opcao = 3
+
+switch (opcao) {
+    case 1:
+        console.log("Opção 1")
+        break;
+    case 2:
+        console.log("Opção 2")
+        break;
+    case 3:
+        console.log("Opção 3")
+        break;
+    case 4:
+        console.log("Opção 4")
+        break;
+    case 5:
+        console.log("Opção 5")
+        break;
+    default:
+        console.log("Opção inválida.")
+        break;
+}
