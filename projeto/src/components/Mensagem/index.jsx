07dce651461d377bdc15a67ad055f0c1a05af555
Sebastian7k <delanemaia@gmail.com>
2026-03-26 16:54:16 -0300

    projeto

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Mensagem () {
+    return (
+        // Comandos HTML.
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Este é um componente com uma mensagem simples.</p>
+        </div>
+    )
+}
+
+export default Mensagem
